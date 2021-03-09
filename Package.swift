9cc113aa1e8ee9ca91f8ea42f57f57bfb2457514
Bilal Benlarbi <bilal@onlyonecard.eu>
2021-03-09 16:19:35 +0100

    SPM support added

--- Package.swift
+++ Package.swift
@@ -1,10 +1,36 @@
+// swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
     name: "PMKAlamofire",
-    dependencies: [
-        .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6),
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
     ],
-    exclude: ["Tests"]
+    products: [
+        .library(
+            name: "PMKAlamofire",
+            targets: ["PMKAlamofire"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/mxcl/PromiseKit.git",
+            .exact("6.13.2")
+        ),
+        .package(
+            url: "https://github.com/Alamofire/Alamofire.git",
+            .exact("5.4.1")
+        )
+    ],
+    targets: [
+        .target(
+            name: "PMKAlamofire",
+            dependencies: ["PromiseKit", "Alamofire"],
+            path: "Sources"
+        )
+    ]
 )
